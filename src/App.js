1ac3923e9f30658c4fa447467293236db4ff0bc2
Aun <aunsetthawut@gmail.com>
2022-01-09 23:46:57 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { ConnectedRouter } from "connected-react-router";
+import Routes from "./routes";
+import Navigation from "./components/Navigation";
+import "./styles/App.scss";
+const App = ({ history }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConnectedRouter history={history}>
+      <div className="App">
+        <Navigation />
+      </div>
+      {/* <Routes /> */}
+    </ConnectedRouter>
   );
-}
+};
+
+// App.propTypes = {
+//   history: PropTypes.object,
+// };
 
 export default App;
